a7a246af7ba3fed98bfba4692c7589af2f633d6c
thanhdnh <myhoangthanh@yahoo.com>
2022-10-17 14:20:14 +0700

    OKKKK

--- Program.cs
+++ Program.cs
@@ -14,15 +14,36 @@ public class Program{
             sum += v;
         return sum;
     }
+    static void InitArray2D(out Array arr, int[] len, int[] low){
+        arr = Array.CreateInstance(typeof(int), len, low);
+    }
+    static void GenerateArray2D(Array arr){
+        Random r = new Random();
+        for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++)
+            for(int j=arr.GetLowerBound(1); j<=arr.GetUpperBound(1); j++)
+                arr.SetValue(r.Next(1, 99), i, j);
+    }
+    static void PrintArray2D(Array arr){
+        for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++){
+            for(int j=arr.GetLowerBound(1); j<=arr.GetUpperBound(1); j++)
+                System.Console.Write("{0, 5}", arr.GetValue(i, j));
+            System.Console.WriteLine();
+        }
+    }
     public static void Main(){
         Console.Clear();
 
-        Array arr;
+        int[] len = {3, 4};
+        int[] low = {1, 1};
+        Array arr2;
+        InitArray2D(out arr2, len, low); GenerateArray2D(arr2); PrintArray2D(arr2);
+
+        /*Array arr;
         InitArray1D(out arr, 5, 0);
         GenerateArray1D(arr);
         foreach(int v in arr)
             System.Console.Write(v + " ");
-        System.Console.WriteLine("\nTong la: " + SumArray1D(arr));
+        System.Console.WriteLine("\nTong la: " + SumArray1D(arr));*/
 
         /*Array arr = Array.CreateInstance(typeof(int), 5);
         arr.SetValue(99, 1);
