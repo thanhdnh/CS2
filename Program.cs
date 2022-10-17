74cf6ea3021304812daa67fa94ca8ccdbd691c90
thanhdnh <myhoangthanh@yahoo.com>
2022-10-17 13:30:48 +0700

    OKK

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿public class Program{
+﻿using System.Collections;
+public class Program{
     public static void Main(){
-        
+        Console.Clear();
+
+        Array arr = Array.CreateInstance(typeof(int), 5);
+        arr.SetValue(99, 1);
+        System.Console.WriteLine(arr.GetValue(1));
+
+        Console.ReadLine();
     }
 }
