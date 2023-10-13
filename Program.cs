f57834982f035a2f2f35c0f29566908ff3829741
thanhdnh <myhoangthanh@yahoo.com>
2023-10-13 14:56:07 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -1,134 +1,71 @@
 ﻿using System;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.Clear();
-        //Bai 4. Kiem tra nam nhuan
-        /*Console.WriteLine("Nhập vào 1 số: ");
-        int nam = int.Parse(Console.ReadLine());
-        if (nam % 4 == 0 && nam % 100 != 0 )
+        static void Main(string[] args)
         {
-          Console.WriteLine("Nam do la nam nhuan");
+                Console.Clear();
+
+                //S1 for
+                Console.WriteLine("moi ban nhap so n");
+                int n = int.Parse(Console.ReadLine());
+                int sum = 0;
+                int i = 1;
+                for (i = 1; i <= (2 * n + 1); i = i + 2)
+                {
+                        sum = sum + i;
+                }
+                Console.WriteLine("for: " + sum);
+
+                //S1 while
+                sum = 0;
+                i = 1;
+                while (i <= 2 * n + 1)
+                {
+                        sum += i;
+                        i = i + 2;
+                }
+                Console.WriteLine("while: " + sum);
+
+                //S1 do while 
+                sum = 0;
+                i = 1;
+                do
+                {
+                        sum = sum + i;
+                        i += 2;
+                }
+                while (i <= 2 * n + 1);
+                Console.WriteLine("do...while: " + sum);
+
+                //S2 for
+                double sum2 = 0;
+                for (i = 1; i <= n; i++)
+                {
+                        sum2 += Math.Pow(i, 2);
+                }
+                Console.WriteLine("For: S2 = " + sum2);
+
+                //S2 while
+                sum = 0;
+                i = 1;
+                while (i <= n)
+                {
+                        sum += i * i;
+                        i++;
+                }
+                Console.WriteLine("while S2: " + sum);
+
+                //S2 do while
+                sum2 = 0;
+                i = 1;
+                do
+                {
+                        sum2 += Math.Pow(i, 2);
+                        i = i + 1;
+                }
+                while (i <= n);
+                Console.WriteLine("do...while: S2 = " + sum2);
+
+                Console.ReadKey();
         }
-         else
-        {
-          Console.WriteLine("Nam do khong phai la nam nhuan");
-        }*/
-
-        //Bai 8. Kiem tra diem thuoc goc phan tu
-        /*float x, y;
-        Console.WriteLine("Hay nhap vao hoanh do x: ");
-        x = float.Parse(Console.ReadLine());
-        Console.WriteLine("Hay nhap vao tung do y: ");
-        y = float.Parse(Console.ReadLine());
-        if ((x == 0) && (y == 0))
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam o goc toa do O", x,y);
-        else if (x == 0)
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren truc tung", x, y);
-        else if (y == 0)
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren truc hoanh", x, y);
-        else if ((x > 0) && (y > 0))
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren phan tu thu I", x, y);
-        else if ((x < 0) && (y >0))
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren phan tu thu II", x, y);
-        else if ((x < 0) && (y < 0))
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren phan tu thu III", x, y);
-        else if ((x > 0) && (y < 0))
-                Console.WriteLine("Diem co toa do ({0}, {1}) nam tren phan tu thu IV", x, y);
-        */
-
-        //Bai 9. Tim so lon nhat trong 3 so
-        /*Console.WriteLine("Nhap 3 so: ");
-        float a = float.Parse(Console.ReadLine());
-        float b = float.Parse(Console.ReadLine());
-        float c = float.Parse(Console.ReadLine());
-        if (a >= b && a >= c)
-                Console.WriteLine("So lon nhat la: {0}", a);
-        else
-        {
-                if (b>=c)
-                        Console.WriteLine("So lon nhat la: {0}", b);
-                else
-                        Console.WriteLine("So lon nhat la: {0}", c);
-        }*/
-
-        //Bai 12. Kiem tra loai tam giac
-        /*Console.Write("Nhap vao canh a:");
-        float a = float.Parse(Console.ReadLine());
-        Console.Write("Nhap vao canh b:");
-        float b = float.Parse(Console.ReadLine());
-        Console.Write("Nhap vao canh c:");
-        float c =  float.Parse(Console.ReadLine());
-        if(a<=0 || b<=0 || c<=0)
-            Console.WriteLine("Khong phai la 3 canh TG");
-        else if(a+b>c && b+c>a && c+a>b){
-            if(a==b && b==c)
-                Console.WriteLine("Tam giac deu");
-            else if(a==b || b==c || a==c){
-                if(a*a+b*b==c*c || b*b+c*c==a*a || c*c+a*a==b*b)
-                    Console.WriteLine("Tam giac vuong can");
-                else
-                    Console.WriteLine("Tam giac can");
-            }else{
-                if(a*a+b*b==c*c || b*b+c*c==a*a || c*c+a*a==b*b)
-                    Console.WriteLine("Tam giac vuong");
-                else
-                    Console.WriteLine("Tam giac thuong");
-            }
-        }else
-            System.Console.WriteLine("Khong phai la 3 canh TG");
-        */
-
-        //Bai 15. Kiem tra nguyen am/phu am
-        /*char ch = Console.ReadKey().KeyChar;
-        Console.WriteLine("\n----");
-        switch(ch){
-                case 'e': case 'u': case 'o': case 'a': case 'i': 
-                        Console.WriteLine("{0} la nguyen am", ch);
-                        break;
-                case 'q': case 'w': case 'r': case 't': case 'y':
-                case 'p': case 's': case 'd': case 'f': case 'g':
-                case 'h': case 'j': case 'k': case 'l': case 'z':
-                case 'x': case 'c': case 'v': case 'b': case 'n':
-                case 'm':
-                        Console.WriteLine("{0} la phu am", ch);
-                        break;
-                default:
-                        System.Console.WriteLine("{0} khong la nguyen/phu am", ch);
-                        break;
-        }*/
-
-        //Bai 21. Tao menu tinh dien tich cac hinh: t, c, g
-        LapLai:
-        System.Console.WriteLine("Nhap vao ki tu [t=Tinh DTHT, c=Tinh DTHCN, g=Tinh DTHTG, x=Thoat]");
-        char luachon = Console.ReadKey().KeyChar;
-        switch(luachon){
-                case 't':
-                        //tinh dien tich hinh tron
-                        Console.Clear();
-                        System.Console.WriteLine("\nDien tich HT");
-                        goto LapLai;
-                case 'c':
-                        //tinh dien tich hcn
-                        Console.Clear();
-                        System.Console.WriteLine("\nDien tich HCN");
-                        goto LapLai;
-                case 'g':
-                        //tinh dien tich hinh tg
-                        Console.Clear();
-                        System.Console.WriteLine("\nDien tich TG");
-                        goto LapLai;
-                case 'x':
-                        //Thoat
-                        Console.Clear();
-                        System.Console.WriteLine("\nThoat...");
-                        return;
-                default:
-                        //quay lai menu
-                        goto LapLai;
-        }
-
-        Console.ReadKey();
-    }
 }
