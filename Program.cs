ebb1a95e92d522e1149a189746d32ae43291db01
thanhdnh <myhoangthanh@yahoo.com>
2023-10-27 16:25:09 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -1,102 +1,104 @@
-﻿using System;
+using System;
 class Program
 {
-        static void Main(string[] args)
-        {
-                Console.Clear();
-
-                //S1 for
-                Console.WriteLine("moi ban nhap so n");
-                int n = int.Parse(Console.ReadLine());
-                int sum = 0;
-                int i = 1;
-                for (i = 1; i <= (2 * n + 1); i = i + 2)
-                {
-                        sum = sum + i;
-                }
-                Console.WriteLine("for: " + sum);
-
-                //S1 while
-                sum = 0;
-                i = 1;
-                while (i <= 2 * n + 1)
-                {
-                        sum += i;
-                        i = i + 2;
-                }
-                Console.WriteLine("while: " + sum);
-
-                //S1 do while 
-                sum = 0;
-                i = 1;
-                do
-                {
-                        sum = sum + i;
-                        i += 2;
-                }
-                while (i <= 2 * n + 1);
-                Console.WriteLine("do...while: " + sum);
-
-                //S2 for
-                double sum2 = 0;
-                for (i = 1; i <= n; i++)
-                {
-                        sum2 += Math.Pow(i, 2);
-                }
-                Console.WriteLine("For: S2 = " + sum2);
-
-                //S2 while
-                sum = 0;
-                i = 1;
-                while (i <= n)
-                {
-                        sum += i * i;
-                        i++;
-                }
-                Console.WriteLine("while S2: " + sum);
-
-                //S2 do while
-                sum2 = 0;
-                i = 1;
-                do
-                {
-                        sum2 += Math.Pow(i, 2);
-                        i = i + 1;
-                }
-                while (i <= n);
-                Console.WriteLine("do...while: S2 = " + sum2);
-
-                //=============Kiem tra so nguyen to
-                bool nt = true;
-                if(n==2)
-                        nt = true;
+        //Khai báo hàm
+        static double TinhTong(double a, double b){
+            double result = a + b;
+            return result;
+        }
+        static void InHoTen(string ho, string ten){
+            Console.WriteLine("Sinh viên [{0}] [{1}].", ho, ten);
+        }
+        static double TinhDTTG(double a, double b, double c){
+            double p = (a+b+c)/2;
+            double area = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            return area;
+        }
+        static void GiaiPTBac1(double a, double b){//ax+b=0
+            if(a==0)
+                if(b==0)
+                    System.Console.WriteLine("VSN");
                 else
-                        for(i=2; i<=Math.Sqrt(n); i++)
-                                if(n%i==0){
-                                        nt = false;
-                                        break;
-                                }else
-                                        nt = nt && true;
-                if(nt)
-                        System.Console.WriteLine("{0} la NT", n);
-                else
-                        System.Console.WriteLine("{0} khong la NT", n);
-                
-                //=======Tong S=9+99+...+999...9
-                //C1. Cong xau
-                string s = "";
-                ulong S1 = 0;
-                for(i=1; i<=n; i++){
-                        s+="9";
-                        S1+=ulong.Parse(s);
-                        //System.Console.WriteLine(s);
-                }
-                System.Console.WriteLine(S1);
+                    System.Console.WriteLine("VN");
+            else{
+                double x = -b/a;
+                System.Console.WriteLine("Nghiem: {0}", x);
+            }
+        }
+        //S=1/2+1/3+...+1/n , n>=1
+        static double TinhTongN(int n){
+            double result = 0;
+            for(int i=1; i<=n; i++)
+                result += 1/(double)i;//result += 1.0/i;
+            return result;
+        }
+        static void NoSwap(int a, int b){
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static void SwapRef(ref int a, ref int b){
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static void SwapOut(out int a, out int b){
+            a = 1; b = 2;
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static void Sort(ref int a, ref int b, ref int c){
+            int min = Math.Min(Math.Min(a, b), c);
+            int max = Math.Max(Math.Max(a, b), c);
+            int r = (a+b+c)-min-max;
+            a = min;
+            b = r;
+            c = max;
+        }
+        static void TinhSC(float R, out float S, 
+                                        out float C){
+            S = (float)Math.PI*R*R;
+            C = 2*(float)Math.PI*R;
+        }
+        static void Main(string[] args){
+            Console.Clear();
+            float R = 2.3f;
+            float SS, CC;
+            TinhSC(R, out SS, out CC);
+            Console.WriteLine($"Voi R={R}, thi S={SS}, C={CC}");
 
-                //C2. 999=10^3-1
-                //C3. 999=9*10^2+9*10^1+9*10^0
-                //C4. Dung log
-                
-                Console.ReadKey();
+            /*int a = 7, b = 9, c = 2;
+            Sort(ref a, ref b, ref c);
+            Console.WriteLine("a={0}, b={1}, c={2}",a,b,c);*/
+
+            //int a = 5, b = 6;
+            //SwapRef(ref a, ref b);
+            //SwapOut(out a, out b);
+            //System.Console.WriteLine(a+", "+b);
+            /*double a = 3.4, b = 5.6;
+            GiaiPTBac1(a, b);
+
+            int n = 10;
+            System.Console.WriteLine("Tong{0}: {1}", n, TinhTongN(n));
+            */
+            /*Console.WriteLine("Nhap 3 canh TG: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Dien tich TG: {TinhDTTG(a, b, c)}");*/
+
+            /*System.Console.WriteLine("Nhap x = , y = ");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}",x,y,TinhTong(x, y));
+
+            System.Console.WriteLine("Nhập họ: ");
+            string h = Console.ReadLine();
+            System.Console.WriteLine("Nhập tên: ");
+            string t = Console.ReadLine();
+            InHoTen(h, t);*/
+
+            Console.ReadLine();
         }
 }
