0b87e492a9520c5b2543d44f46b7ed22c8db6112
thanhdnh <myhoangthanh@yahoo.com>
2023-11-03 16:04:18 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -53,12 +53,39 @@ class Program
                 result[index++] = v;
         return result;
     }
+    static void SortArr(int[] a){
+        for(int i=0; i<a.Length-1; i++)
+            for(int j=i+1; j<a.Length; j++)
+                if(a[i]>a[j]){
+                    int t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+    }
+    static bool CapSoCong(int[] a, out double d){
+        d = a[1] - a[0];
+        for(int i=1; i<a.Length; i++)
+            if(a[i]-a[i-1]!=d){
+                d = double.PositiveInfinity;
+                return false;
+            }
+        return true;
+    }
     static void Main(string[] args){
         Console.Clear();
-        int[] a = new int[]{3, 4, 3, 9, 3, 4, 6};
-        int [] c = FilterArr(a);
+        int[] a = new int[]{1, 3, 4, 7};
+        double d;
+        if(CapSoCong(a, out d)){
+            Console.WriteLine("Cap so cong");
+            Console.WriteLine("Cong sai {0}", d);
+        }else
+            Console.WriteLine("Khong phai!");
+        /*int [] c = FilterArr(a);
         foreach(int v in c)
-            Console.Write(v+" ");
+            Console.Write(v+" ");*/
+        //SortArr(a);
+        /*foreach(int v in a)
+            Console.Write(v+" ");*/
         //Console.WriteLine(KTDXMang2(a));
         
         Console.ReadLine();
