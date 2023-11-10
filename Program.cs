d2d5f9de318e645a430a33620a406df328bed515
thanhdnh <myhoangthanh@yahoo.com>
2023-11-10 14:44:32 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -4,16 +4,34 @@ class Program
 {
     static void NhapTT1NV(object[] nv)
     {
-        Console.WriteLine("Nhập họ tên nhân viên:");
+        Console.WriteLine(" + Nhập họ tên nhân viên:");
         nv[0] = Console.ReadLine();
-        Console.WriteLine("Nhập hệ số lương: ");
+        Console.WriteLine(" + Nhập hệ số lương: ");
         nv[1] = float.Parse(Console.ReadLine());
-        Console.WriteLine("Nhập phụ cấp theo ngày: ");
+        Console.WriteLine(" + Nhập phụ cấp theo ngày: ");
         nv[2] = int.Parse(Console.ReadLine());
     }
     static string XuatTT1NV(object[] nv)
     {
         return $"Nhân viên: {nv[0]}, Hệ số lương: {nv[1]}, Phụ cấp ngày: {nv[2]}";
+    }
+    static void NhapTTCacNV(object[][] nvs){
+        for(int i=0; i<nvs.Length; i++){
+            Console.WriteLine($">> Nhập thông tin nhân viên {i}");
+            NhapTT1NV(nvs[i]);
+        }
+    }
+    static string XuatTTCacNV(object[][] nvs){
+        string result = "";
+        for(int i=0; i<nvs.Length; i++)
+            result += XuatTT1NV(nvs[i]) + "\n\n";
+        return result;
+    }
+    static object[][] KhoiTaoMangNV(int n){
+        object[][] nvs = new object[n][];
+        for(int i=0; i<nvs.Length; i++)
+            nvs[i] = new object[3];
+        return nvs;
     }
     public static void Main(string[] args)
     {
@@ -21,9 +39,9 @@ class Program
         Console.InputEncoding = Encoding.UTF8;
         Console.Clear();
 
-        object[] nv = new object[3];
-        NhapTT1NV(nv);
-        XuatTT1NV(nv);
+        object[][] nvs = KhoiTaoMangNV(3);
+        NhapTTCacNV(nvs);
+        Console.WriteLine(XuatTTCacNV(nvs));
 
         Console.ReadLine();
     }
