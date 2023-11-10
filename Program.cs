e55f7da958b1083c385ff2bd52ee918efb40429c
thanhdnh <myhoangthanh@yahoo.com>
2023-11-10 14:22:33 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -1,107 +1,30 @@
-using System;
+﻿using System.Text;
+
 class Program
 {
-    static void GenerateMatrix(int[,] a, int d1, int d2){
-        a = new int[d1, d2];
-        Random r = new Random();
-        for(int i=0; i<d1; i++)
-            for(int j=0; j<d2; j++){
-                a[i,j] = r.Next();
-            }
+    static void NhapTT1NV(object[] nv)
+    {
+        Console.WriteLine("Nhập họ tên nhân viên:");
+        nv[0] = Console.ReadLine();
+        Console.WriteLine("Nhập hệ số lương: ");
+        nv[1] = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập phụ cấp theo ngày: ");
+        nv[2] = int.Parse(Console.ReadLine());
     }
-    static void PrintMatrix(int[,] a){
-        
+    static string XuatTT1NV(object[] nv)
+    {
+        return $"Nhân viên: {nv[0]}, Hệ số lương: {nv[1]}, Phụ cấp ngày: {nv[2]}";
     }
-
-    static int[,] SumMatrices(int[,] a, int[,] b){
-
-    }
-    static bool KTDXMang(int[] a){
-        for(int i=0; i<a.Length/2; i++){
-            if(a[i]!=a[a.Length-i-1])
-                return false;
-        }
-        return true;
-    }
-    static bool KTDXMang2(int[] a){
-        int[] b = new int[a.Length/2+1];
-        for(int i=0; i<=a.Length/2; i++)
-            b[i] = a[i]-a[a.Length-i-1];
-        for(int i=0; i<b.Length; i++)
-            if(b[i]!=0)
-                return false;
-        return true;
-    }
-    static int TongMang(int[] a){
-        int T = 0;
-        for(int i=0; i<a.Length; i++)
-            if(a[i]%2!=0)
-                T += a[i];
-        return T;
-    }
-    static int TongMang2(int[] a){
-        int T = 0;
-        foreach(int v in a)
-            if(v%2!=0)
-                T += v;
-        return T;
-    }
-    static bool Exist(int x, int[] arr){
-        for(int i=0; i<arr.Length; i++)
-            if(arr[i]==x)
-                return true;
-        return false;
-    }
-    static int[] FilterArr(int[] a){
-        int[] b = new int[a.Length];
-        for(int i=0; i<b.Length; i++)
-            b[i] = int.MaxValue;
-        int index = 0;
-        for(int i=0; i<a.Length; i++)
-            if(!Exist(a[i], b))
-                b[index++] = a[i];
-        int[] result = new int[index];
-        index = 0;
-        foreach(int v in b)
-            if(v!=int.MaxValue)
-                result[index++] = v;
-        return result;
-    }
-    static void SortArr(int[] a){
-        for(int i=0; i<a.Length-1; i++)
-            for(int j=i+1; j<a.Length; j++)
-                if(a[i]>a[j]){
-                    int t = a[i];
-                    a[i] = a[j];
-                    a[j] = t;
-                }
-    }
-    static bool CapSoCong(int[] a, out double d){
-        d = a[1] - a[0];
-        for(int i=1; i<a.Length; i++)
-            if(a[i]-a[i-1]!=d){
-                d = double.PositiveInfinity;
-                return false;
-            }
-        return true;
-    }
-    static void Main(string[] args){
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
         Console.Clear();
-        int[] a = new int[]{1, 3, 4, 7};
-        double d;
-        if(CapSoCong(a, out d)){
-            Console.WriteLine("Cap so cong");
-            Console.WriteLine("Cong sai {0}", d);
-        }else
-            Console.WriteLine("Khong phai!");
-        /*int [] c = FilterArr(a);
-        foreach(int v in c)
-            Console.Write(v+" ");*/
-        //SortArr(a);
-        /*foreach(int v in a)
-            Console.Write(v+" ");*/
-        //Console.WriteLine(KTDXMang2(a));
-        
+
+        object[] nv = new object[3];
+        NhapTT1NV(nv);
+        XuatTT1NV(nv);
+
         Console.ReadLine();
     }
 }
